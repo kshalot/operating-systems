4927fb6c00037f8e8d551555d120cc982d76464d
Karol Szuster <kszuster@protonmail.com>
2019-03-18 00:01:36 +0100

    finished ex1

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -4,11 +4,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/times.h>
+#include <string.h>
+
 
 int generate(int quantity, int size, char* file) {
 
     unsigned char *buffer = malloc(quantity * size * sizeof(char));
-    srand(time(NULL));
 
     int i;
     for(i = 0; i < quantity * size; i++) {
@@ -17,15 +19,15 @@ int generate(int quantity, int size, char* file) {
     }
     int fd = open(file, O_CREAT | O_WRONLY, 0777); 
     if(fd == -1) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file\n");
         exit(-1);
     }
     if(write(fd, buffer, quantity * size * sizeof(char)) < 0) {
-        fprintf(stderr, "Error while writing to file");
+        fprintf(stderr, "Error while writing to file\n");
         exit(-1);
     }
     if(close(fd) == -1) {
-        fprintf(stderr, "Error while closing file");
+        fprintf(stderr, "Error while closing file\n");
         exit(-1);
     }
     free(buffer);
@@ -39,17 +41,16 @@ int sys_copy(int quantity, int size, char* source, char* destination) {
     int source_fd = open(source, O_RDONLY);
     int destination_fd = open(destination, O_CREAT | O_WRONLY, 0777);
     if(source_fd == -1|| destination_fd == -1) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file\n");
         exit(-1);
     }
-
     int i;
     for(i = 0; i < quantity; i++) {
         read(source_fd, buffer, size * sizeof(char));
         write(destination_fd, buffer, size * sizeof(char));
     }
     if(close(source_fd) == -1 || close(destination_fd) == -1) {
-        fprintf(stderr, "Error while closing file");
+        fprintf(stderr, "Error while closing file\n");
         exit(-1);
     }
     free(buffer);
@@ -63,17 +64,16 @@ int lib_copy(int quantity, int size, char* source, char* destination) {
     FILE *source_f = fopen(source, "r");
     FILE *destination_f = fopen(destination, "w");
     if(source_f == NULL || destination_f == NULL) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file\n");
         exit(-1);
     }
-
     int i;
     for(i = 0; i < quantity; i++) {
         fread(buffer, sizeof(char), size, source_f);
         fwrite(buffer, sizeof(char), size, destination_f);
     }
     if(fclose(source_f) == -1 || fclose(destination_f) == -1) {
-        fprintf(stderr, "Error while closing file");
+        fprintf(stderr, "Error while closing file\n");
         exit(-1);
     }
     free(buffer);
@@ -85,7 +85,7 @@ int sys_sort(int quantity, int size, char *filename) {
     char *buffer_b = malloc(size * sizeof(char));
     int fd = open(filename, O_RDWR);
     if(fd == -1) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file\n");
         exit(-1);
     }
     int i, j;
@@ -112,7 +112,7 @@ int sys_sort(int quantity, int size, char *filename) {
         write(fd, buffer_b, size * sizeof(char));
     }
     if(close(fd) == -1) {
-        fprintf(stderr, "Error closing file");
+        fprintf(stderr, "Error closing file\n");
         exit(-1);
     }
     free(buffer_a);
@@ -125,7 +125,7 @@ int lib_sort(int quantity, int size, char *filename) {
     char *buffer_b = malloc(size * sizeof(char));
     FILE *file = fopen(filename, "r+");
     if(file == NULL) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file\n");
         exit(-1);
     }
     int i, j;
@@ -152,12 +152,12 @@ int lib_sort(int quantity, int size, char *filename) {
         fwrite(buffer_b, sizeof(char), size, file);
 
         if(ferror(file) != 0) {
-            fprintf(stderr, "Error while writing to file");
+            fprintf(stderr, "Error while writing to file\n");
             exit(-1);
         }
     }
     if(fclose(file) != 0) {
-        fprintf(stderr, "Error closing file");
+        fprintf(stderr, "Error closing file\n");
         exit(-1);
     }
     free(buffer_a);
@@ -165,15 +165,89 @@ int lib_sort(int quantity, int size, char *filename) {
     return 0;
 }
 
-int main() {
-    int quantity = 10;
-    int size = 10;
-    generate(quantity, size, "system.txt");
-    sys_sort(quantity, size, "system.txt");
-    generate(quantity, size, "lib.txt");
-    lib_sort(quantity, size, "lib.txt");
-    sys_copy(quantity, size, "system.txt", "syscopy.txt");
-    lib_copy(quantity, size, "lib.txt", "libcopy.txt");
+double time_elapsed(clock_t begin, clock_t end) {
+    return (double) (end - begin) / sysconf(_SC_CLK_TCK);
+}
 
+void print_time(char* command, struct tms tms_start, struct tms tms_end) {
+    printf("%s\n", command);
+    printf("User:   %.5lf s   ", time_elapsed(tms_start.tms_utime, tms_end.tms_utime));
+    printf("System: %.5lf s\n\n", time_elapsed(tms_start.tms_stime, tms_end.tms_stime));
+    printf("----------------------------------------------\n");
+}
+
+int main(int argc, char **argv) {
+    if(argc == 1) {
+        fprintf(stderr, "Too few arguments\n");
+        exit(-1);
+    }
+    int i;
+    char command[200];
+    srand(time(NULL));
+
+    struct tms* start = malloc(sizeof(struct tms*));
+    struct tms* end = malloc(sizeof(struct tms*));
+
+    for(i = 1; i < argc; i++) {
+        times(start);
+        if(strcmp(argv[i], "generate") == 0) {
+            if(i + 3 >= argc) {
+                fprintf(stderr, "Too few arguments\n");
+                exit(-1);
+            }
+            char *filename = argv[++i];
+            int quantity = atoi(argv[++i]);
+            int size = atoi(argv[++i]);
+            generate(quantity, size, filename);
+            sprintf(command, "generate %s %s %s", argv[i-2], argv[i-1], argv[i]);
+        }
+        else if(strcmp(argv[i], "sort") == 0) {
+            if(i + 4 >= argc) {
+                fprintf(stderr, "Too few arguments\n");
+                exit(-1);
+            }
+            char *filename = argv[++i];
+            int quantity = atoi(argv[++i]);
+            int size = atoi(argv[++i]);
+            if(strcmp(argv[++i], "sys") == 0) {
+                sys_sort(quantity, size, filename);
+            }
+            else if(strcmp(argv[++i], "lib") == 0) {
+                lib_sort(quantity, size, filename);
+            }
+            else {
+                fprintf(stderr, "Wrong option! Use \"sys\" or \"lib\"");
+                exit(-1);
+            }
+            sprintf(command, "sort %s %s %s %s", argv[i-3], argv[i-2], argv[i-1], argv[i]);
+        }
+        else if(strcmp(argv[i], "copy") == 0) {
+            if(i + 5 >= argc ) {
+                fprintf(stderr, "Too few arguments\n");
+                exit(-1);
+            }
+            char *source = argv[++i];
+            char *destination = argv[++i];
+            int quantity = atoi(argv[++i]);
+            int size = atoi(argv[++i]);
+            if(strcmp(argv[++i], "sys") == 0) {
+                sys_copy(quantity, size, source, destination);
+            }
+            else if(strcmp(argv[++i], "lib") == 0) {
+                lib_copy(quantity, size, source, destination);
+            }
+            else {
+                fprintf(stderr, "Wrong option! Use \"sys\" or \"lib\"");
+                exit(-1);
+            }
+            sprintf(command, "copy %s %s %s %s", argv[i-3], argv[i-2], argv[i-1], argv[i]);
+        }
+        else {
+            fprintf(stderr, "Invalid argument %s\n", argv[i]);
+            exit(-1);
+        }
+        times(end);
+        print_time(command, start, end);
+    }
     return 0;
 }
