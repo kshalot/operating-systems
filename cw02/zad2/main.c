c05ac3be425c60474fc7837025f42c829912b8c6
Karol Szuster <kszuster@protonmail.com>
2019-03-18 14:32:49 +0100

    minor bugfix

--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -7,27 +7,22 @@
 #include <dirent.h>
 #include <string.h>
 #include <time.h>
+#include <ftw.h>
 
-int time_comparison(struct tm *x, struct tm *y) {
-    // x < y => -1
-    // x == y => 0
-    // x > y => 1
-    if(x->tm_year == y->tm_year && x->tm_mon == y->tm_mon && x->tm_mday == y->tm_mday)
-        return 0;
-    if(x->tm_year > y->tm_year)
-        return 1;
-    if(x->tm_year == y->tm_year && x->tm_mon > y->tm_mon)
-        return 1;
-    if(x->tm_year == y->tm_year && x->tm_mon == y->tm_mon && x->tm_mday > y->tm_mday)
-        return 1;
-    return -1;
+struct tm *reference_date;
+char mode;
+
+int print_file(struct stat file_stat, const char *path) {
+    
+    if((mode == '<' && localtime(&(file_stat.st_mtime)) < reference_date) ||
+       (mode == '=' && localtime(&(file_stat.st_mtime)) == reference_date) ||
+       (mode == '>' && localtime(&(file_stat.st_mtime)) > reference_date)) {
+            printf("%s\n", path);
+    }
+    return 0;
 }
 
-int print_file(struct stat file_stat, char *path) {
-    printf("%s\n", path);
-}
-
-int search_directory(char *dir_path, struct tm *reference_date, char mode) {
+int search_directory(char *dir_path) {
     DIR *directory = opendir(dir_path);
     if(directory == NULL) {
         fprintf(stderr, "Error opening directory\n");
@@ -35,24 +30,19 @@ int search_directory(char *dir_path, struct tm *reference_date, char mode) {
     }
     struct stat file_stat;
     struct dirent *entry;
-    char *absolute_dir_path = realpath(dir_path, NULL);
 
     while((entry = readdir(directory)) != NULL) {
         if(strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".") == 0) {
             continue;
         }
-        char *entry_path = malloc(strlen(absolute_dir_path) + strlen(entry->d_name) + 1 /*nullbyte*/ + 1 /*slash*/);
-        sprintf(entry_path, "%s/%s", absolute_dir_path, entry->d_name);
+        char *entry_path = malloc(strlen(dir_path) + strlen(entry->d_name) + 1 /*nullbyte*/ + 1 /*slash*/);
+        sprintf(entry_path, "%s/%s", dir_path, entry->d_name);
         if(lstat(entry_path, &file_stat) == 0) {
             if(S_ISDIR(file_stat.st_mode)) {
-                search_directory(entry_path, reference_date, mode);
+                search_directory(entry_path);
             }
             else {
-                if((mode == '<' && time_comparison(localtime(&(file_stat.st_mtime)), reference_date) == -1) ||
-                   (mode == '=' && time_comparison(localtime(&(file_stat.st_mtime)), reference_date) == 0) ||
-                   (mode == '>' && time_comparison(localtime(&(file_stat.st_mtime)), reference_date) == 1)) {
-                    print_file(file_stat, entry_path);
-                }
+                print_file(file_stat, entry_path);
             }
         }
         free(entry_path);
@@ -60,19 +50,33 @@ int search_directory(char *dir_path, struct tm *reference_date, char mode) {
     return 0;
 }
 
+int nftw_fn(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+    print_file(*sb, fpath);
+    return 0;
+}
+
 int main(int argc, char **argv) {
-    if(argc < 4) {
+    if(argc < 5) {
         fprintf(stderr, "Too few arguments\n");
         exit(-1);
     }
     char *path = argv[1];
+    char *absolute_path = realpath(path, NULL);
+    
     char *unparsed_date = argv[2];
-    char mode = argv[3][0];
+    mode = argv[3][0];
+    char *walk_type = argv[4];
 
-    printf("Searching in %s for date %s in mode %c\n", path, unparsed_date, mode);
+    reference_date = malloc(sizeof(struct tm));
+    strptime(unparsed_date, "%Y-%m-%d", reference_date);
 
-    struct tm *parsed_date = malloc(sizeof(struct tm));
-    strptime(unparsed_date, "%Y-%m-%d", parsed_date);
-
-    search_directory(path, parsed_date, mode);
+    if(strcmp(walk_type, "stat") == 0) {
+        search_directory(absolute_path);
+    }
+    else if(strcmp(walk_type, "nftw") == 0) {
+        nftw(absolute_path, &nftw_fn, 100, FTW_PHYS);
+    }
+    else {
+        fprintf(stderr, "Invalid walk type! Use \"nftw\" or \"stat\"");
+    }
 }
