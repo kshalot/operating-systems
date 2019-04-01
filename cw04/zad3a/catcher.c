40ea3d1154555fa10394bcf1f881e5ff4a13353d
Karol Szuster <kszuster@protonmail.com>
2019-04-01 17:21:36 +0200

    major progress

--- cw04/zad3a/catcher.c
+++ cw04/zad3a/catcher.c
@@ -4,13 +4,43 @@
 #include <signal.h>
 #include <string.h>
 
+int signal_count = 0;
+
+void catch_kill(int sig, siginfo_t *info, void *context) {
+    if(sig == SIGUSR1)
+        signal_count++;
+    else if(sig == SIGUSR2) {
+        int i;
+        for(i = 0; i < signal_count; i++)
+            kill(info->si_pid, SIGUSR1);
+        kill(info->si_pid, SIGUSR2);
+        exit(0);
+    }
+}
+
 int main(int argc, char **argv) {
     if(argc != 2) {
-        fprintf(stderr, "Invalid arguments. Usage 'catcher <mode>'");
+        fprintf(stderr, "Invalid arguments. Usage 'catcher <mode>'\n");
         return -1;
     }
-    if(strcmp(mode, "KILL") == 0) {
+    char *mode = argv[1];
 
+    if(strcmp(mode, "KILL") == 0) {
+        sigset_t signals;
+        sigfillset(&signals);
+        sigdelset(&signals, SIGUSR1);
+        sigdelset(&signals, SIGUSR2);
+        sigprocmask(SIG_BLOCK, &signals, NULL);
+
+        struct sigaction action;
+        action.sa_sigaction = catch_kill;
+        sigemptyset(&action.sa_mask);
+        action.sa_flags = SA_SIGINFO;
+
+        if(sigaction(SIGUSR1, &action, NULL) == -1 || sigaction(SIGUSR2, &action, NULL) == -1) {
+            fprintf(stderr, "An error occurred\n");
+            return -1;
+        }
     }
     else if(strcmp(mode, "SIGQUEUE") == 0) {
 
@@ -22,6 +52,8 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Invalid mode! Choose KILL/SIGQUEUE/SIGRT\n");
         return -1;
     }
+
+    printf("PID: %d\n", getpid());
 
     while(1) {
         pause();
--- cw04/zad3a/sender.c
+++ cw04/zad3a/sender.c
@@ -4,10 +4,28 @@
 #include <signal.h>
 #include <string.h>
 
-int signal_count;
+int signal_count = 0;
+int expected_count;
 
-void handle_kill(int signum) {
+void handle_kill(int sig, siginfo_t *info, void *context) {
+    if(sig == SIGUSR1)
         signal_count++;
+    else if(sig == SIGUSR2) {
+        printf("Sent %d out of %d signals\n", signal_count, expected_count);
+        exit(0);
+    }
+}
+
+void handle_queue(int sig, siginfo_t *info, void *context) {
+    if(sig == SIGUSR1)
+        signal_count++;
+    else if(sig == SIGUSR2) {
+
+    }
+}
+
+void handle_sigrt(int sig, siginfo_t *info, void *context) {
+    
 }
 
 int main(int argc, char **argv) {
@@ -17,14 +35,14 @@ int main(int argc, char **argv) {
         return -1;
     }
     int pid = atoi(argv[1]);
-    signal_count = atoi(argv[2]);
+    expected_count = atoi(argv[2]);
     char *mode = argv[3];
 
     if(strcmp(mode, "KILL") == 0) {
         sigset_t signals;
         sigfillset(&signals);
         sigdelset(&signals, SIGUSR1);
-        sigdelset(&signals, SIGUSR1);
+        sigdelset(&signals, SIGUSR2);
         sigprocmask(SIG_BLOCK, &signals, NULL);
 
         struct sigaction action;
@@ -37,10 +55,15 @@ int main(int argc, char **argv) {
             return -1;
         }
         int i;
-        for(i = 0; i < signal_count; i++) {
-            kill(pid, SIGUSR1);
+        for(i = 0; i < expected_count; i++)
+            if(kill(pid, SIGUSR1) != 0) {
+                fprintf(stderr, "Signal SIGUSR1 wasn't sent properly\n");
+                exit(-1);
+            }
+        if(kill(pid, SIGUSR2) != 0) {
+            fprintf(stderr, "Signal SIGURS2 wasn't sent properly\n");
+            exit(-1);
         }
-        kill(pid, SIGUSR2);
     }
     else if(strcmp(mode, "SIGQUEUE")) {
 
@@ -49,7 +72,7 @@ int main(int argc, char **argv) {
 
     }
     else {
-        fprintf("Invalid mode! Choose KILL/SIGRT/SIGQUEUE");
+        fprintf(stderr, "Invalid mode! Choose KILL/SIGRT/SIGQUEUE");
         return -1;
     }
 
