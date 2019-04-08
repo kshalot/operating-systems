1b5e3e2d691651e420e251d4a012c33949a22d09
Karol Szuster <kszuster@protonmail.com>
2019-04-08 21:06:20 +0200

    major bugfix

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -14,51 +14,6 @@ typedef struct {
   char **argv;
 } command;
 
-
-int read_file(char *filename, char **buffer) {
-  FILE *file = fopen(filename, "r");
-  if(file == NULL)
-    return -1;
-
-  fseek(file, 0, SEEK_END);
-  int size = ftell(file);
-  fseek(file, 0, SEEK_SET);
-
-  *buffer = malloc((size + 1) * sizeof(char));
-
-  return fread(*buffer, 1, size, file);
-}
-
-command parse_line(char *line) {
-  char *saveptr;
-  char **args = malloc((MAX_ARGS + 1) * sizeof(char));
-  char *arg = strtok_r(line, " ", &saveptr);
-  command cmd = {};
-
-  int i;
-  for(i = 0; arg != NULL && i < MAX_ARGS; i++, arg = strtok_r(NULL, " ", &saveptr)) {
-    if (i == 0)
-      cmd.name = arg;
-    args[i] = arg;  
-  }
-  cmd.argv = args;
-  
-  return cmd;
-}
-
-command  *parse_file(char *file_content, int *size) {
-  char *saveptr;
-  command *cmds = malloc((MAX_COMMANDS + 1) * sizeof(command));
-  char *cmd = strtok_r(file_content, "|\n", &saveptr);
-
-  int i;
-  for(i = 0; cmd != NULL && i < MAX_COMMANDS; i++, cmd = strtok_r(NULL, "|\n", &saveptr))
-    cmds[i] = parse_line(cmd);
-
-  *size = i;
-  return cmds;
-}
-
 int execute(command *cmds, int size) {
 
   int pipes[2][2];
@@ -66,7 +21,6 @@ int execute(command *cmds, int size) {
   int i;
   for(i = 0; i < size; i++) {
     command cmd = cmds[i];
-    printf("mam command: %s\n", cmd.name);
 
     if(i != 0) {
       close(pipes[i%2][0]);
@@ -90,8 +44,67 @@ int execute(command *cmds, int size) {
   close(pipes[i%2][1]);
 
   return 0;
-
 }
+
+int read_file(char *filename, char **buffer) {
+  FILE *file = fopen(filename, "r");
+  if(file == NULL)
+    return -1;
+
+  fseek(file, 0, SEEK_END);
+  int size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  *buffer = malloc((size + 1) * sizeof(char));
+
+  return fread(*buffer, 1, size, file);
+}
+
+command parse_command(char *command_string) {
+  char *saveptr;
+  char **args = malloc((MAX_ARGS + 1) * sizeof(char));
+  char *arg = strtok_r(command_string, " ", &saveptr);
+  command cmd = {};
+
+  int i;
+  for(i = 0; arg != NULL && i < MAX_ARGS; i++, arg = strtok_r(NULL, " ", &saveptr)) {
+    if (i == 0)
+      cmd.name = arg;
+    args[i] = arg;  
+  }
+  cmd.argv = args;
+  
+  return cmd; 
+}
+
+command *parse_line(char *line, int *size) {
+  char *saveptr;
+  command *cmds = malloc(MAX_COMMANDS * sizeof(command));
+  char *command_string = strtok_r(line, "|", &saveptr);
+  
+  int i;
+  for(i = 0; command_string != NULL && i < MAX_COMMANDS; i++, command_string = strtok_r(NULL, "|", &saveptr))
+    cmds[i] = parse_command(command_string);
+  
+  *size = i;
+  return cmds;
+}
+
+command  *parse_file(char *file_content) {
+  char *saveptr;
+  command *cmds = malloc((MAX_COMMANDS + 1) * sizeof(command));
+  char *cmd = strtok_r(file_content, "\n", &saveptr);
+
+  int i;
+  for(i = 0; cmd != NULL && i < MAX_COMMANDS; i++, cmd = strtok_r(NULL, "\n", &saveptr)) {
+    int size;
+    cmds = parse_line(cmd, &size);
+    execute(cmds, size);
+  }
+
+  return cmds;
+}
+
 
 int main(int argc, char **argv) {
   if(argc != 2) {
@@ -101,10 +114,7 @@ int main(int argc, char **argv) {
 
   char *file_content;
   read_file(argv[1], &file_content);
-  int size;
-  command *cmds = parse_file(file_content, &size);
-
-  execute(cmds, size);
+  parse_file(file_content);
 
   free(file_content);
   
